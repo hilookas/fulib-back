357af058e50f2be7f333c118b0696bb12d438cd5
lookas <i@18kas.com>
2022-11-04 19:30:11 +0800

    Inital typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'sqlite',
+        database: configService.get<string>('DATABASE')
+          ? configService.get<string>('DATABASE')
+          : join(process.cwd(), 'database.sqlite'),
+        autoLoadEntities: true,
+        synchronize: configService.get<string>('DATABASE') ? undefined : true,
+      })
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
